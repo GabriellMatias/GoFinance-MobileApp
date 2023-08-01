61f3bf50c4d2807757cb483aa1a8883000e5051c
GabriellMatias <gabriel.goiasat@gmail.com>
2023-08-01 15:24:42 -0300

    :x: Error: Updating files trying to solve styled-components inferType error

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Dashboard } from './src/Screens/Dashboard'
+import { ThemeProvider } from 'styled-components/native'
+import { theme } from './src/global/styles/theme'
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
+}
---
+++ src/Screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native'
+import { Container } from './style'
+import React from 'react'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  )
+}
